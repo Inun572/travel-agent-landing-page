089349176c0ee57e22fdfff52df324fc6beff851
Inun572 <helmi.ainun@gmail.com>
2023-03-08 22:38:38 +0700

    update fitur autoslide

--- script.js
+++ script.js
@@ -13,3 +13,26 @@ const validateForm = () => {
     alert("Pesan Terkirim. Terimakasih telah menghubungi kami");
   }
 };
+
+var slideIndex = 1;
+
+let imgList = document.getElementsByClassName('img-slide');
+
+const slider = () => {
+  // imgList[slideIndex].classList.add('active');
+  if (slideIndex > imgList.length-1) {
+    slideIndex = 0;
+  } else {
+    imgList[slideIndex].classList.add('active');
+    console.log(imgList[slideIndex]);
+    slideIndex++;
+  }
+
+  imgList[slideIndex].classList.remove('active');
+
+  console.log(imgList[slideIndex]);
+}
+
+setInterval( () => {
+  slider();
+}, 3000)
