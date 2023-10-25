bb0be7e69e3d3596283595a01fe6b7a507916b77
Inun572 <helmi.ainun@gmail.com>
2023-10-25 13:17:11 +0700

    upgrade: add styling on scrolled navbar

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// validasi form
 const validateForm = () => {
   const nama = document.getElementById('nama').value;
   const email = document.getElementById('email').value;
@@ -14,6 +15,7 @@ const validateForm = () => {
   }
 };
 
+// auto slide
 var slideIndex = 1;
 
 let imgList = document.getElementsByClassName('img-slide');
@@ -36,7 +38,21 @@ setInterval(() => {
   slider();
 }, 3000);
 
+// hamburger button
 var navMenu = document.querySelector('.nav-menu');
 const showMenu = () => {
   navMenu.classList.toggle('show');
 };
+
+// navbar behaviour
+
+const navbar = document.querySelector('.navbar');
+const hero = document.querySelector('.hero');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY >= hero.scrollHeight) {
+    navbar.classList.add('onScroll');
+  } else if (window.scrollY < hero.scrollHeight) {
+    navbar.classList.remove('onScroll');
+  }
+});
